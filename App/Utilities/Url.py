a807221dadcd8300b5d059a571c391ea58f385d1
yazdancode <yshabanei@gmail.com>
2025-09-13 11:16:21 +0330

    i added template to project

--- App/Utilities/Url.py
+++ App/Utilities/Url.py
@@ -1,7 +1,9 @@
-from configs.Config import Config
-
-
 class Url:
     @staticmethod
-    def base_url() -> str:
+    def current() -> str:
         return Config.BASE_URL
+
+    @staticmethod
+    def current_route() -> str:
+        uri = os.environ.get('REQUEST_URI', '/')
+        return uri.split('?')[0]
--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from App.Utilities.Url import Url
+
+
+route = Url.current_route()
+
+
+if route == '/colors/blue':
+    'template/colors/blue.html'
+
+if route == '/colors/green':
+    'template/colors/green.html'
+
+if route == '/colors/red':
+    'template/colors/red.html'
 
